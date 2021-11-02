a82496797811a19a14f6048445ec81e54cb7a94a
Owen Eldridge <oweneldridge7@gmail.com>
2021-11-02 17:33:31 -0400

    "initialized babel"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const foo = {
   bar: 1,
   foobar: 2,
-  barfoo: 3
+  barfoo: 3,
 };
 
 const foo2 = {
   ...foo,
   soap: 1,
-  shampoo: 4
+  shampoo: 4,
+  bar: 2,
 };
 console.log(foo);
 console.log(foo2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
 module.exports = {
   mode: "development",
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
   devServer: {
-    contentBase: "./dist"
+    static: "./dist",
   },
 };
