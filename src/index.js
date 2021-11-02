4e1c32ccda886a3b963aebf132c9785cbdc86ef1
Owen Eldridge <oweneldridge7@gmail.com>
2021-11-02 16:28:53 -0400

    fixed webpack setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const foo = {
-    bar: 1,
-    foobar: 2,
-    barfoo: 3
-}
+  bar: 1,
+  foobar: 2,
+  barfoo: 3
+};
 
+const foo2 = {
+  ...foo,
+  soap: 1,
+  shampoo: 4
+};
 console.log(foo);
+console.log(foo2);
