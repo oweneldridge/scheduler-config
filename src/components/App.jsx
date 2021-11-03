6c72b58eb914c3f787bd00c05e7f0b195e218433
Owen Eldridge <oweneldridge7@gmail.com>
2021-11-03 19:57:19 -0400

    "fix ul icon"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
           <h1>hello there, react!! </h1>
         </section>
         <img src={background} alt="background" width="250" />
-        <img src={background} alt="background" width="250" />
+        <img src={background} alt="background1" width="250" />
         <Lists />
       </main>
     </>
